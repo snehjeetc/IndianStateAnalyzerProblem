2ed8bc5c6b11d6481727c41cb5bfafb85cda0540
snehjeetc12 <csnehjeet12@gmail.com>
2021-03-23 01:00:35 +0530

    Test method added to test exception handling of wrong type file for loadStateCodeData method

--- src/test/java/com/statecensusproblem/StateCodeAnalyzerTestClass.java
+++ src/test/java/com/statecensusproblem/StateCodeAnalyzerTestClass.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.io.IOException;
+
 public class StateCodeAnalyzerTestClass {
     private CensusAnalyzer censusAnalyzer;
 
@@ -12,6 +14,8 @@ public class StateCodeAnalyzerTestClass {
             = "/home/snehjeetc12/IdeaProjects/StateCensusProblem/src/main/resources/IndiaStateCode.csv";
     private final static String WRONG_PATH
             = "/home/snehjeetc12/IdeaProjects/StateCensusProblem/src/test/resources/IndiaStateCode.csv";
+    private static final String WRONG_FILE_TYPE
+            = "/home/snehjeetc12/IdeaProjects/StateCensusProblem/src/main/resources/pom.xml";
 
     @Before
     public void init(){
@@ -35,4 +39,17 @@ public class StateCodeAnalyzerTestClass {
             Assert.assertEquals(CensusAnalyzerException.ExceptionType.FILE_NOT_FOUND, e.type);
         }
     }
+
+    @Test
+    public void givenStateCensusCSVFile_IfGivenFileIsCorrect_NotOfWrongType_ShouldThrowCustomExceptionOfWrongType() throws IOException {
+        try {
+            ExpectedException exceptionrule = ExpectedException.none();
+            exceptionrule.expect(CensusAnalyzerException.class);
+            censusAnalyzer.loadCensusData(WRONG_FILE_TYPE);
+        } catch (CensusAnalyzerException e) {
+            e.printStackTrace();
+            Assert.assertEquals(CensusAnalyzerException.ExceptionType.WRONG_FILE_TYPE, e.type);
+        }
+    }
+
 }
