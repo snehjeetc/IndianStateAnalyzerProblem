5802d54226ab72322f73d78f0f29877dde9d1aee
snehjeetc12 <csnehjeet12@gmail.com>
2021-03-22 20:00:51 +0530

    Added Exception handling for corrupt file types as csv

--- src/main/java/com/statecensusproblem/CensusAnalyzer.java
+++ src/main/java/com/statecensusproblem/CensusAnalyzer.java
@@ -6,14 +6,17 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class CensusAnalyzer {
     public int loadCensusData(String path) throws CensusAnalyzerException{
         int i = 0;
+
+        Reader reader = null;
         try{
-            Reader reader = Files.newBufferedReader(Paths.get(path));
+            reader = Files.newBufferedReader(Paths.get(path));
             CsvToBeanBuilder<StateCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<StateCensusCSV>(reader);
             csvToBeanBuilder.withType(StateCensusCSV.class)
                             .withIgnoreLeadingWhiteSpace(true);
@@ -23,8 +26,13 @@ public class CensusAnalyzer {
                 StateCensusCSV stateCensusCSV = censusCSVIterator.next();
                 i++;
             }
-        } catch (IOException e) {
+            reader.close();
+        }catch(NoSuchFileException e){
             throw new CensusAnalyzerException("File not found", CensusAnalyzerException.ExceptionType.FILE_NOT_FOUND);
+        }catch(RuntimeException e){
+            throw new CensusAnalyzerException("Unable to read file", CensusAnalyzerException.ExceptionType.CORRUPT_FILE_TYPE);
+        }catch(IOException e){
+            throw new CensusAnalyzerException("Something went wrong", CensusAnalyzerException.ExceptionType.IO_EXCEPTION);
         }
         return i;
     }
--- src/main/java/com/statecensusproblem/CensusAnalyzerException.java
+++ src/main/java/com/statecensusproblem/CensusAnalyzerException.java
@@ -2,7 +2,9 @@ package com.statecensusproblem;
 
 public class CensusAnalyzerException extends Exception {
     enum ExceptionType{
-        FILE_NOT_FOUND;
+        FILE_NOT_FOUND,
+        CORRUPT_FILE_TYPE,
+        IO_EXCEPTION;
     }
     ExceptionType type;
     public CensusAnalyzerException(String message, ExceptionType type){
